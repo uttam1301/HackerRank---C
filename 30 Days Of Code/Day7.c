d822aff0fe07a8d6489aff53eab7341e44838329
Uttam Kumar Saha <74055109+uttam1301@users.noreply.github.com>
2021-01-19 10:20:27 +0530

    Arrays

---
+++ 30 Days Of Code/Day7.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n, i, A[1000];
+    
+    scanf("%d",&n);
+
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&A[i]);
+    }
+
+    for(i=n-1;i>=0;i--)
+    {
+        printf("%d ",A[i]);
+    }
+
+    return 0;
+}

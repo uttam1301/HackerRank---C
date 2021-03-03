d3d04f015e07f3b298244ebc38a3ac1c2416febc
Uttam Kumar Saha <74055109+uttam1301@users.noreply.github.com>
2021-03-03 23:13:52 +0530

    Plus Minus

---
+++ Algorithms/problem5.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+void plusMinus(vector<int> arr) 
+{
+    int p=0,n=0,z=0,i,size=arr.size();
+    for(i=0;i<size;i++)
+    {
+        if(arr[i]>0)
+        p++;
+        else if(arr[i]<0)
+        n++;
+        else 
+        z++;
+    }
+    double pos,neg,zero;
+    pos = (double)p/size;
+    neg = (double)n/size;
+    zero = (double)z/size;
+    cout<<setprecision(6)<<pos<<endl<<neg<<endl<<zero;
+    
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split_string(arr_temp_temp);
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    plusMinus(arr);
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
